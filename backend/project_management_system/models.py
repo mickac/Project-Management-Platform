627318a64e250dabeb211a7e2aa457b0c1f55d15
mickac <kaczynski.mp@gmail.com>
2022-01-01 22:37:08 +0100

    Init for models

--- backend/project_management_system/models.py
+++ backend/project_management_system/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    details = models.CharField(max_length=500)
+    startDate = models.DateField()
+    endDate = models.DateField()
+    status = models.CharField(max_length=15)
