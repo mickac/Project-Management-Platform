1ba6a97991f917682bd37aa5b105f8e5a3985d55
mickac <kaczynski.mp@gmail.com>
2022-01-02 18:35:38 +0100

    Adding projects list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const projectItems =[
+  { 
+    id: 1,
+    title: "test",
+    details: "test",
+    start_date: "2022-01-02",
+    end_date: "2022-01-09",
+    status: 2,
+  },
+]
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewInProgress: true,
+      projectList: projectItems,
+    };
+  }
+
+  displayStatus = (statusCheck) => {
+    if (statusCheck === 0) {
+      return this.setState({ 
+        viewCompleted: true,
+        viewInProgress: false,
+        viewNew: false,
+        status: 0,
+       });
+    }
+    if (statusCheck === 1) {
+      return this.setState({
+        viewCompleted: false,
+        viewInProgress: true,
+        viewNew: false,
+        status: 1,
+       });
+    }
+    if (statusCheck === 2){
+      return this.setState({
+        viewCompleted: false,
+        viewInProgress: false,
+        viewNew: true,
+        status: 2,
+       });
+    }
+  };
+
+  renderTabList = () => {
+    return (
+      <div className="nav nav-tabs">
+        <span
+          className={this.state.viewCompleted ? "nav-link active" : "nav-link"}
+          onClick={() => this.displayStatus(0)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Completed
+        </span>
+        <span
+          className={this.state.viewInProgress ? "nav-link active" : "nav-link"}
+          onClick={() => this.displayStatus(1)}
+        >
+          In progress
+        </span>
+        <span
+          className={this.state.viewNew ? "nav-link active" : "nav-link"}
+          onClick={() => this.displayStatus(2)}
+        >
+          New
+        </span>
+      </div>
+    );
+  };
+
+  renderItems = () => {
+    const newItems = this.state.projectList.filter(
+      (item) => item.status === this.state.status
+    );
+
+    return (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Project name</TableCell>
+              <TableCell align="right">Start date</TableCell>
+              <TableCell align="right">End date</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right">Operations</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {newItems.map((item) =>  (
+              <TableRow
+                key={item.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {item.title}
+                </TableCell>
+                <TableCell align="right">{item.start_date}</TableCell>
+                <TableCell align="right">{item.end_date}</TableCell>
+                <TableCell align="right">{item.status}</TableCell>
+                <TableCell align="right">TBD</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  };
+
+  render() {
+    return (
+      <main className="container">
+        <h1 className="text-center my-4">Project Management Platform</h1>
+        <div className="row">
+          <div className="col-md-10 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="mb-4">
+                <button
+                  className="btn btn-primary"
+                >
+                  Add Project
+                </button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush border-top-0">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default App;
