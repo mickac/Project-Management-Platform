66051205f2e06c023572f4e68ece8c6c1e0c9f84
mickac <kaczynski.mp@gmail.com>
2022-01-02 01:05:22 +0100

    Expanding models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "phonenumber_field",
     'project_management_system',
 ]
 
--- backend/project_management_system/models.py
+++ backend/project_management_system/models.py
@@ -1,9 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from phonenumber_field.modelfields import PhoneNumberField
+
+GEDNDER_CHOICES = [
+    ('Male', 'Female'),
+    ('Not specified'),
+]
 
 
-class Book(models.Model):
+class Project(models.Model):
     title = models.CharField(max_length=50)
     details = models.CharField(max_length=500)
-    startDate = models.DateField()
-    endDate = models.DateField()
+    start_date = models.DateField()
+    end_date = models.DateField()
     status = models.CharField(max_length=15)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.TextField(max_length=15, choices=GEDNDER_CHOICES)
+    birth_date = models.DateField(null=True, blank=True)
+    phone_number= models.CharField(max_length=15, required=False)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
