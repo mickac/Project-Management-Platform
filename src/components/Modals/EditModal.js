e2cbf6d31024b5bcf023fa3a56f726f943485f3a
Michał Kaczyński <34127259+mickac@users.noreply.github.com>
2022-01-23 15:30:52 +0100

    Date validation fix for EditModal

--- src/components/Modals/EditModal.js
+++ src/components/Modals/EditModal.js
@@ -8,6 +8,13 @@ function EditModal({ activeItem, toggleEdit, onClose, onSave }) {
     setIsEditConfirm((current) => !current);
   };
   const [errors, setErrors] = useState({});
+  const validateDate = (start_date, end_date) => {
+    if (start_date !== "" && end_date !== "" && end_date >= start_date) {
+      setErrors({ ...errors, date_range: false });
+    } else {
+      setErrors({ ...errors, date_range: true });
+    }
+  };
   return (
     <Modal
       show={toggleEdit}
@@ -30,7 +37,7 @@ function EditModal({ activeItem, toggleEdit, onClose, onSave }) {
             onChange={(e) => {
               if (
                 /^[a-zA-Z0-9 ]*$/.test(e.target.value) &&
-                e.target.value != ""
+                e.target.value !== ""
               ) {
                 setCurrentItem({ ...currentItem, title: e.target.value });
                 setErrors({ ...errors, title: false });
@@ -59,7 +66,7 @@ function EditModal({ activeItem, toggleEdit, onClose, onSave }) {
             onChange={(e) => {
               if (
                 /^[a-zA-Z0-9 ]*$/.test(e.target.value) &&
-                e.target.value != ""
+                e.target.value !== ""
               ) {
                 setCurrentItem({ ...currentItem, details: e.target.value });
                 setErrors({ ...errors, details: false });
@@ -86,21 +93,12 @@ function EditModal({ activeItem, toggleEdit, onClose, onSave }) {
             defaultValue={activeItem.start_date}
             disabled={isEditConfirm}
             onChange={(e) => {
-              if (e.target.value != "") {
-                setCurrentItem({ ...currentItem, start_date: e.target.value });
-                setErrors({ ...errors, start_date: false });
-              } else {
-                setErrors({ ...errors, start_date: true });
-              }
+              setCurrentItem({ ...currentItem, start_date: e.target.value });
+              validateDate(e.target.value, currentItem.end_date);
             }}
-            isValid={!errors.start_date}
-            isInvalid={errors.start_date}
+            isValid={!errors.date_range}
+            isInvalid={errors.date_range}
           />
-          {!errors.start_date ? null : (
-            <Form.Text className="text-danger">
-              Start date cannot be empty.
-            </Form.Text>
-          )}
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label>End date</Form.Label>
@@ -112,22 +110,15 @@ function EditModal({ activeItem, toggleEdit, onClose, onSave }) {
             disabled={isEditConfirm}
             onChange={(e) => {
               setCurrentItem({ ...currentItem, end_date: e.target.value });
-              if (
-                currentItem.start_date >= e.target.value &&
-                e.target.value != ""
-              ) {
-                setCurrentItem({ ...currentItem, details: e.target.value });
-                setErrors({ ...errors, end_date: false });
-              } else {
-                setErrors({ ...errors, end_date: true });
-              }
+              validateDate(currentItem.start_date, e.target.value);
             }}
-            isValid={!errors.end_date}
-            isInvalid={errors.end_date}
+            isValid={!errors.date_range}
+            isInvalid={errors.date_range}
           />
-          {!errors.end_date ? null : (
+          {!errors.date_range ? null : (
             <Form.Text className="text-danger">
-              End date must be greater than start date and cannot be empty.
+              Either start date or end date is empty or start date is greater
+              than end date.
             </Form.Text>
           )}
         </Form.Group>
